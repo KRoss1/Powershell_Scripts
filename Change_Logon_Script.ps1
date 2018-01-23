674f7fda46ba24b5c7555b9067b426a3e10e68b4
KRoss1 <KRoss1@users.noreply.github.com>
2018-01-23 10:00:54 +0000

    kr

--- Change_Logon_Script.ps1
+++ Change_Logon_Script.ps1
@@ -1,3 +1,7 @@
-﻿$username = Read-Host "Enter Username"
-$script = Read-Host "Enter Logon Script"
-Get-ADUser $username | Set-ADUser -scriptpath $script
+﻿param(
+[String]$Username,
+[String]$LogonScript
+)
+
+
+Get-ADUser $Username | Set-ADUser -scriptpath $LogonScript
