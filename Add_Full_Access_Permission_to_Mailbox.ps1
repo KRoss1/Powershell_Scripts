5c1801d3b58f53e7fd921236c6df5a4f4c09d5e4
KRoss1 <KRoss1@users.noreply.github.com>
2018-01-24 16:44:44 +0000

    kr

--- Add_Full_Access_Permission_to_Mailbox.ps1
+++ Add_Full_Access_Permission_to_Mailbox.ps1
@@ -12,11 +12,20 @@ Specifies the Mailbox you want to add permission to
 .PARAMETER User
 Specifies the User you want to add to the mailbox
 
-.EXAMPLE
-Add_Full_Access_Permission_Mailbox -Mailbox "Accounts" -User "Mike"
+.PARAMETER Server
+Specifies the Server the mailbox is on
+
+.PARAMETER ExchangeUsername
+Specifies the Admin Credentials to use
+
+.PARAMETER Password
+Specifies the Admin Credentials Password to use
 
 .EXAMPLE
-Reset_AD_User_Password.ps1 -Mailbox Engineering -User mike.jones
+Add_Full_Access_Permission_to_Mailbox.ps1 -Mailbox "Accounts" -User "Mike" -Server Exchange1 -ExchangeUsername Domain\ExchangeAdmin -Password P@ssword1
+
+.EXAMPLE
+Add_Full_Access_Permission_to_Mailbox.ps1 -Mailbox Engineering -User mike.jones -Server Exchange1 -ExchangeUsername Domain\ExchangeAdmin -Password P@ssword1
 
 
 #>
@@ -25,7 +34,20 @@ param(
     [Parameter(Mandatory=$true)]
         [String]$Mailbox,
     [Parameter(Mandatory=$true)]
-        [SecureString]$User
+        [SecureString]$User,
+    [Parameter(Mandatory=$true)]
+        [String]$Server,
+    [Parameter(Mandatory=$true)]
+        [String]$ExchangeUsername,
+    [Parameter(Mandatory=$true)]
+        [SecureString]$Password    
+    
 )
 
-Add-MailboxPermission -Identity $Mailbox -User $User -AccessRights FullAccess
+$ScriptBlock = {
+    Add-MailboxPermission -Identity $args[0] -User $args[1] -AccessRights FullAccess
+}
+
+$credential = New-Object -TypeName System.Management.Automation.PSCredential $ExchangeUsername,$Password
+
+Invoke-Command -ComputerName $Server -Credential $credential -ScriptBlock $ScriptBlock -ArgumentList $Mailbox, $User
