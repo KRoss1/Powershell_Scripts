9499d85a413cde410bd2aaa44f8bff2852b99f54
KRoss1 <KRoss1@users.noreply.github.com>
2018-03-02 15:33:27 +0000

    Kr

---
+++ Get_Where_SMTPAddress_is_Used.ps1
@@ -0,0 +1,31 @@
+<#
+.AUTHOR
+Kieran Ross
+
+.SYNOPSIS
+Finds Where SMTP Address is being used
+
+.DESCRIPTION
+Uses Get-Recipient to find where a certain SMTP Address is used
+
+
+.PARAMETER EmailAddress
+Email Address you want to find
+
+
+.EXAMPLE
+.\Get_Where_SMTPAddress_is_Used.ps1 -EmailAddress "Info@Domain.co.uk"
+
+
+#>
+
+[CmdletBinding()]Param(
+    [Parameter(Mandatory=$true)]
+        [String]$EmailAddress
+)
+
+Write-Verbose "Adding Exchange Commands"
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+Write-Verbose "Searching for Email Address: $EmailAddress"
+Get-Recipient | Select Name -Expand EmailAddresses | Where {$_.SMTPAddress -match "$EmailAddress"} | Format-Table Name, SMTPAddress
